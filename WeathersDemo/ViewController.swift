a8ca7ebe3febc98ace0d3afe2b93b27857a3e8b1
Tran Chung <tranchung927@gmail.com>
2017-06-12 18:30:18 +0700

    Init

---
+++ WeathersDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeathersDemo
+//
+//  Created by Tran Chung on 6/12/17.
+//  Copyright © 2017 Tran Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLb: UILabel!
+    @IBOutlet weak var conditonLb: UILabel!
+    @IBOutlet weak var degreLb: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
