27cbba2977d62b75522ed5d1dc53fc9af7a5d3ef
Tran Chung <tranchung927@gmail.com>
2017-06-13 13:52:30 +0700

    commit for Chung!

--- WeathersDemo/ViewController.swift
+++ WeathersDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var nameLb: UILabel!
@@ -16,8 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var degreLb: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var degree: Int!
+    var condition: String!
+    var imgURL: String!
+    var city: String!
+    var exists: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,7 +31,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let urlRequest = URLRequest(url: URL(string: "http://api.apixu.com/v1/current.json?key=19c25d79bf234b12af0101146171206&q=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))")!)
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if error == nil {
+                do {
+                    
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                    
+                    if let current = json["current"] as? [String : AnyObject] {
+                        if let temp = current["temp_c"] as? Int {
+                            self.degree = temp
+                        }
+                        if let condition = current["condition"] as? [String : AnyObject] {
+                            self.condition = condition["text"] as! String
+                            self.imgURL = condition["icon"] as! String
+                        }
+                    }
+                    if let location = json["location"] as? [String : AnyObject] {
+                        self.city = location["name"] as? String
+                    }
+                    
+                    if let _ = json["error"] {
+                        self.exists = false
+                    }
+                    DispatchQueue.main.async {
+                        if self.exists {
+                            self.degreLb.text = self.degreLb.description
+                        }
+                    }
+                } catch let jsonError {
+                    print(jsonError.localizedDescription)
+                }
+            }
+        }
+    }
 
 }
 
